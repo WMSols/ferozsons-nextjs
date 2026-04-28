0dc394ee3ece9f527851ca25fa5b62befd0677f4
Sohaib Zaman <insaaninsaan004@gmail.com>
2026-04-28 16:34:23 +0500

    Cloudflare runtime error fix

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -1,4 +1,4 @@
-
+ export const runtime = 'edge';
 import PageHero from "@/components/layout/PageHero";
 import { whyWorkWithUs } from "@/data/careers";
 import WhyWorkCard from "./components/WhyWorkCard";
